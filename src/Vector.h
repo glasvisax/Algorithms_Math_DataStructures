d4267a62842e2550dce4a68559039e0513b0b4bf
glasvisax <1icyscarecrow@gmail.com>
2024-08-22 18:09:26 +0300

    Vector

---
+++ src/Vector.h
@@ -0,0 +1,304 @@
+#pragma once
+#include <memory>
+#include <type_traits>
+#include <iterator>
+
+template <typename T, typename Alloc = std::allocator<T>>
+class Vector
+{
+	size_t size = 0;
+	size_t capacity = 0;
+	T*	   arr = nullptr;
+	Alloc  alloc;
+
+	template <bool isConst>
+	class BaseIterator
+	{
+		using difference_type = int;
+		using value_type = std::conditional_t<isConst, const T, T>;
+		using pointer = std::conditional_t<isConst, const T*, T*>;
+		using reference = std::conditional_t<isConst, const T&, T&>;
+		using iterator_category = std::random_access_iterator_tag;
+
+		pointer ptr = nullptr;
+
+		friend class Vector<T>;
+
+		BaseIterator(pointer _ptr)
+			: ptr(_ptr)
+		{
+		}
+
+	public:
+		reference operator*()
+		{
+			return *ptr;
+		}
+
+		pointer operator->()
+		{
+			return ptr;
+		}
+
+		BaseIterator& operator++()
+		{
+			++ptr;
+			return *this;
+		}
+
+		BaseIterator operator++(int)
+		{
+			pointer oldptr = ptr;
+			++ptr;
+
+			return BaseIterator(oldptr);
+		}
+
+		BaseIterator& operator--()
+		{
+			--ptr;
+			return *this;
+		}
+
+		BaseIterator operator--(int)
+		{
+			pointer oldptr = ptr;
+			--ptr;
+
+			return BaseIterator(oldptr);
+		}
+
+		BaseIterator& operator+=(difference_type value)
+		{
+			ptr += value;
+			return *this;
+		}
+
+		BaseIterator& operator-=(difference_type value)
+		{
+			ptr -= value;
+			return *this;
+		}
+
+		template <bool Cond>
+		bool operator!=(BaseIterator<Cond>& other)
+		{
+			return ptr != other.ptr;
+		}
+
+		template <bool Cond>
+		bool operator==(BaseIterator<Cond>& other)
+		{
+			return ptr == other.ptr;
+		}
+
+		operator BaseIterator<true>()
+		{
+			return { ptr };
+		}
+	};
+
+	void destroy()
+	{
+		for (size_t i = 0; i < size; ++i)
+		{
+			std::allocator_traits<Alloc>::destroy(alloc, arr + i);
+		}
+		std::allocator_traits<Alloc>::deallocate(alloc, arr, capacity);
+	}
+
+public:
+	using ConstIterator = BaseIterator<true>;
+	using Iterator = BaseIterator<false>;
+
+	Vector() = default;
+
+	Vector(const Vector& other)
+	{
+		arr = std::allocator_traits<Alloc>::allocate(alloc, other.capacity);
+		capacity = other.capacity;
+		for (size_t i = 0; i < other.size; ++i)
+		{
+			std::allocator_traits<Alloc>::construct(alloc, arr + i, *(other.arr + i));
+			size++;
+		}
+	}
+
+	Vector(Vector&& other) noexcept
+	{
+		std::swap(arr, other.arr);
+		std::swap(size, other.size);
+		std::swap(capacity, other.capacity);
+	}
+
+	~Vector()
+	{
+		if (capacity)
+		{
+			destroy();
+		}
+	}
+
+	Vector& operator=(const Vector& other) 
+	{
+		if (capacity)
+		{
+			destroy();
+		}
+		arr = std::allocator_traits<Alloc>::allocate(alloc, other.capacity);
+		capacity = other.capacity;
+		for (size_t i = 0; i < other.size; ++i)
+		{
+			std::allocator_traits<Alloc>::construct(alloc, arr + i, *(other.arr + i));
+			size++;
+		}
+	}
+
+	Vector& operator=(Vector&& other)
+	{
+		if (capacity)
+		{
+			destroy();
+		}
+
+		size = 0;
+		capacity = 0;
+		arr = nullptr;
+
+		std::swap(arr, other.arr);
+		std::swap(size, other.size);
+		std::swap(capacity, other.capacity);
+		return *this;
+	}
+
+	T& operator[](size_t index)
+	{
+		return *(arr + index);
+	}
+
+	const T& operator[](size_t index) const
+	{
+		return *(arr + index);
+	}
+
+	T& at(size_t index)
+	{
+		if (index > size - 1)
+		{
+			throw std::out_of_range("out of range");
+		}
+		return *(arr + index);
+	}
+
+	const T& at(size_t index) const
+	{
+		if (index > size - 1)
+		{
+			throw std::out_of_range("out of range");
+		}
+		return *(arr + index);
+	}
+
+	void reserve(size_t amount)
+	{
+		if (capacity >= amount)
+		{
+			return;
+		}
+		T*	   newarr = std::allocator_traits<Alloc>::allocate(alloc, amount);
+		size_t i = 0;
+		if constexpr (!std::is_nothrow_move_constructible_v<T>)
+		{
+			if constexpr (std::is_copy_constructible_v<T>)
+			{
+				try
+				{
+					for (; i < size; ++i)
+					{
+						std::allocator_traits<Alloc>::construct(alloc, newarr + i, *(arr + i));
+					}
+				}
+				catch (...)
+				{
+					for (size_t j = 0; j <= i; ++j)
+					{
+						std::allocator_traits<Alloc>::destroy(alloc, newarr + j);
+					}
+					std::allocator_traits<Alloc>::deallocate(alloc, newarr, i + 1);
+					throw;
+				}
+			}
+			else
+			{
+				for (; i < size; ++i)
+				{
+					std::allocator_traits<Alloc>::construct(alloc, newarr + i, std::move(*(arr + i)));
+				}
+			}
+		}
+		else
+		{
+			for (; i < size; ++i)
+			{
+				std::allocator_traits<Alloc>::construct(alloc, newarr + i, std::move(*(arr + i)));
+			}
+		}
+
+		std::allocator_traits<Alloc>::deallocate(alloc, arr, size);
+		arr = newarr;
+		capacity = amount;
+	}
+
+	void pushBack(const T& value)
+	{
+		emplaceBack(value);
+	}
+
+	void pushBack(T&& value)
+	{
+		emplaceBack(std::move(value));
+	}
+
+	template <typename... Args>
+	void emplaceBack(Args&&... args)
+	{
+		if (size >= capacity)
+		{
+			reserve(capacity < 4 ? 4 : capacity * 2);
+		}
+
+		std::allocator_traits<Alloc>::construct(alloc, arr + size, std::forward<Args>(args)...);
+
+		size++;
+	}
+
+	size_t getSize() const
+	{
+		return size;
+	}
+
+	size_t getCapacity() const
+	{
+		return capacity;
+	}
+
+	Iterator begin()
+	{
+		return Iterator(arr);
+	}
+
+	Iterator end()
+	{
+		return Iterator(arr + size);
+	}
+
+	ConstIterator cbegin() const
+	{
+		return ConstIterator(arr);
+	}
+
+	ConstIterator cend() const
+	{
+		return ConstIterator(arr + size );
+	}
+};
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include "Matrix.h"
 #include "rbmap.h"
 #include "Graph.h"
+#include <cassert>
+#include "Vector.h"
 
 using namespace lin_alg;
 using namespace graph;
@@ -14,28 +16,107 @@ void test_matrix();
 void test_rb_tree();
 void test_graphs();
 
+void runTests()
+{
+	// Тест конструктора по умолчанию
+	Vector<int> vec;
+	assert(vec.getSize() == 0);
+	assert(vec.getCapacity() == 0);
+
+	// Тест pushBack и размера/вместимости
+	vec.pushBack(1);
+	vec.pushBack(2);
+	vec.pushBack(3);
+	vec.pushBack(4);
+	vec.pushBack(5);
+
+	assert(vec.getSize() == 5);
+	assert(vec.getCapacity() >= 5);
+
+	// Тест доступа по индексу
+	assert(vec[0] == 1);
+	assert(vec[1] == 2);
+	assert(vec[2] == 3);
+	assert(vec[3] == 4);
+	assert(vec[4] == 5);
+
+	// Тест метода at() с корректным и некорректным индексом
+	try
+	{
+		vec.at(4) = 10;
+		assert(vec.at(4) == 10);
+	}
+	catch (const std::out_of_range&)
+	{
+		assert(false); // не должно сюда попасть
+	}
+
+	try
+	{
+		vec.at(5);
+		assert(false); // не должно сюда попасть
+	}
+	catch (const std::out_of_range&)
+	{
+		// должно выбросить исключение
+	}
+
+	// Тест конструктора копирования
+	Vector<int> vecCopy(vec);
+	assert(vecCopy.getSize() == vec.getSize());
+	for (size_t i = 0; i < vec.getSize(); ++i)
+	{
+		assert(vecCopy[i] == vec[i]);
+	}
+
+	// Тест конструктора перемещения
+	Vector<int> vecMove(std::move(vecCopy));
+	assert(vecMove.getSize() == 5);
+	assert(vecCopy.getSize() == 0); // после перемещения размер должен быть 0
+
+	// Тест оператора копирования
+	Vector<int> vecAssign = vecMove;
+	assert(vecAssign.getSize() == vecMove.getSize());
+	for (size_t i = 0; i < vecMove.getSize(); ++i)
+	{
+		assert(vecAssign[i] == vecMove[i]);
+	}
+
+	// Тест оператора перемещения
+	Vector<int> vecMoveAssign;
+	vecMoveAssign = std::move(vecAssign);
+	assert(vecMoveAssign.getSize() == 5);
+	assert(vecAssign.getSize() == 0);
+
+	// Тест работы итераторов
+	Vector<int>::Iterator it = vecMoveAssign.begin();
+	assert(*it == 1);
+	++it;
+	assert(*it == 2);
+	it++;
+	assert(*it == 3);
+
+	// Тест работы константных итераторов
+	const Vector<int>&		   constVec = vecMoveAssign;
+	Vector<int>::ConstIterator cit = constVec.cbegin();
+	assert(*cit == 1);
+	++cit;
+	assert(*cit == 2);
+	cit++;
+	assert(*cit == 3);
+
+	// Тест метода reserve()
+	vecMoveAssign.reserve(20);
+	assert(vecMoveAssign.getCapacity() == 20);
+	assert(vecMoveAssign.getSize() == 5);
+
+	std::cout << "All tests passed!" << std::endl;
+}
+
 int main()
 {
-	//test_graphs();
-
-		// unweighted graph
-	UGraph<int> ug;
-	ug.addEdge(1, 2);
-	ug.addEdge(1, 3);
-	ug.addEdge(2, 4);
-	ug.addEdge(4, 7);
-	ug.addEdge(4, 8);
-	ug.addEdge(3, 6);
-	ug.addEdge(3, 5);
-
-	// bfs
-	auto res3 = findPath(1, 7, ug, false);
-
-	for (auto i : res3.first)
-	{
-		std::cout << i << " ";
-	}
-	std::cout << "\n";
+	runTests();
+	return 0;
 }
 
 void test_graphs()
